61651684c38aed0af9ff09c8415499f889e8ea05
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-05 15:25:18 +0200

    Change more css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
 @endsection
 
 @section('content-center')
-  <div class="panel panel-default">
-    <div class="panel-heading">Recently Uploaded</div>
-    <div class="panel-body">
+  <div class="content-pane">
+    <div class="pane-heading">Recently Uploaded</div>
+    <div class="pane-body">
 
     </div>
   </div>
