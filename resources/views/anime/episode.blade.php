160cf9c4fdef382e73691db2fc312e8967c88c00
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-06 23:51:14 +0200

    Create temporary episode page

--- resources/views/anime/episode.blade.php
+++ resources/views/anime/episode.blade.php
@@ -2,5 +2,15 @@
 @section('title', "$show->title - Episode $number")
 
 @section('content-center')
-
+  <div class="content-header">{{ $show->title }} - Episode {{ $number }}</div>
+  <div class="content-generic">
+    <ul>
+      @foreach($videos as $video)
+        <li>
+          <a href="{{ $video->stream_url }}">Video with {{ $video->resolution }} resolution.</a>
+        </li>
+      @endforeach
+    </ul>
+    <div class="content-close"></div>
+  </div>
 @endsection
