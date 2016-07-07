2cd93a560c0d659fb22d32d4e1d8f809eb6edfe9
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-07 03:05:48 +0200

    Set username to be unique

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -49,7 +49,7 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'name' => 'required|max:255|unique:users',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
         ]);
