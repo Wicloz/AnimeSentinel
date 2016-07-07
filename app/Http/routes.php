b4e7fcec9c4823e5f02022cf01abf8a49f3226e0
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-07 02:49:05 +0200

    Add profile and episode controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,16 +13,25 @@
 
 Route::auth();
 
+// Welcome Page
 Route::get('/', 'AnimeController@home');
+
+// Information Pages
 Route::get('/about', 'PagesController@about');
 Route::get('/news', 'PagesController@news');
 
+// Anime Listings
 Route::get('/anime/recent', 'AnimeController@recent');
 Route::get('/anime/search', 'AnimeController@searchEmpty');
 Route::post('/anime/search', 'AnimeController@searchPost');
 
+// Anime Details
 Route::get('/anime/{show}', 'AnimeController@details');
 Route::get('/anime/{show}/{translation_type}/episode-{episode_num}', 'AnimeController@episode');
 
+// Stream Pages
 Route::get('/anime/{show}/{translation_type}/episode-{episode_num}/{video}', 'EpisodeController@stream');
 Route::get('/stream/{video}', 'EpisodeController@stream');
+
+// Profile Pages
+Route::get('/dashboard', 'ProfileController@dashboard');
