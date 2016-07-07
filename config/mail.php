e8a98142e2ed4afb39eecb09cb527546052f3e65
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-07 02:17:40 +0200

    Set mail sender config

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'support@wilcodeboer.me', 'name' => 'Animesentinel Support'],
 
     /*
     |--------------------------------------------------------------------------
