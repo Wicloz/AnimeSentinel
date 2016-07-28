8dca9b50f70fabe835681f705c9d8b41b4c5602b
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-28 18:43:02 +0200

    Add permissions column to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->text('permissions');
             $table->rememberToken();
             $table->timestamps();
         });
