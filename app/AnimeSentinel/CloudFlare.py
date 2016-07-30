67e9bf3e0733cfca4224d28912d6fd815ce55d6f
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-30 17:47:00 +0200

    Allow downloading of cloudflare protected pages

--- app/AnimeSentinel/CloudFlare.py
+++ app/AnimeSentinel/CloudFlare.py
@@ -1,4 +1,9 @@
 import cfscrape
+import sys
+import json
 
-scraper = cfscrape.create_scraper()
-print scraper.get("http://kissanime.to").content
+if __name__ == "__main__":
+  cookie_arg, user_agent = cfscrape.get_cookie_string(sys.argv[1])
+  f = open("../../storage/app/cookies/" + sys.argv[2], "w")
+  json.dump({'cookies': cookie_arg, 'agent': user_agent}, f)
+  f.close()
