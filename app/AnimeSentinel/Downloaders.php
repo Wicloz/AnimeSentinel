84056b85df62c8203e852339c5a0e04f5843e3a9
Wilco de Boer <deboer.wilco@gmail.com>
2016-07-30 11:37:16 +0200

    Create helper file for downloads

---
+++ app/AnimeSentinel/Downloaders.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\AnimeSentinel;
+
+class Downloaders
+{
+  /**
+   * Returns a webpage after executing JavaScript
+   *
+   * @return string
+   */
+  public static function downloadJavaScript($url) {
+    return ""; //TODO
+  }
+
+  /**
+   * Downloads webpages behind a cloudflare filter.
+   * Saves cookies where possible.
+   *
+   * @return string
+   */
+  public static function downloadCloudFlare($url) {
+    return ""; //TODO
+  }
+}
