102792c88056f46ed96ea28dc778039966d14f56
Wilco de Boer <deboer.wilco@gmail.com>
2016-08-05 09:05:32 +0200

    Remove memory flag

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-//php artisan queue:listen --queue=periodic_high,periodic_low,default --timeout=0 --memory=0
+//php artisan queue:listen --queue=periodic_high,periodic_low,default --timeout=0
 
 /*
 |--------------------------------------------------------------------------
