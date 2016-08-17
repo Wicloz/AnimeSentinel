b382142c292571fa1e98b8bce558ec4a8f0f7b37
Wilco de Boer <deboer.wilco@gmail.com>
2016-08-17 12:36:15 +0200

    fix

--- app/BaseModel.php
+++ app/BaseModel.php
@@ -28,7 +28,7 @@ abstract class BaseModel extends Model
         ));
 
         $query->where(DB::raw(
-          '1=1 ORDER BY '.implode(', ', $columns).')'
+          '1=1 ORDER BY '.implode(', ', $columns).') d'
         ), 'like', '');
       break;
 
