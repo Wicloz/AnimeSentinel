17d155d9632232b84459414ad90c478fc7cc9b54
Wilco de Boer <wfh@wilcodeboer.me>
2016-10-03 21:30:07 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@
 
 Auth::routes();
 
+// Post Controller
+Route::post('/anime/add', 'PostController@showAdd');
+
 // Welcome Page
 Route::get('/', 'AnimeController@home');
 
@@ -25,6 +28,22 @@ Route::get('/news', 'PagesController@news'); //TODO
 // Streamers Pages
 Route::get('/streamers', 'StreamersController@list'); //TODO
 Route::get('/streamers/{streamer}', 'StreamersController@details'); //TODO
+
+// Profile Pages
+Route::get('/anime/overview', 'UserController@overview');
+Route::get('/user/settings', 'UserController@settings_general');
+Route::get('/user/notifications/mail', 'UserController@settings_nots_mail');
+
+// Profile Modifications
+Route::post('/user/settings/general', 'Auth\UpdateSettingsController@general');
+Route::post('/user/settings/mal', 'Auth\UpdateSettingsController@mal');
+Route::post('/user/settings/password', 'Auth\UpdateSettingsController@password');
+Route::post('/user/notifications/mail/general', 'Auth\UpdateSettingsController@notifications_mail_general');
+Route::post('/user/notifications/mail/specific', 'Auth\UpdateSettingsController@notifications_mail_specific');
+
+// CSS Routes
+Route::get('/user/settings/css', 'CssController@showUserSettings'); //TODO
+Route::post('/user/settings/css', 'CssController@setUserSettings'); //TODO
 
 // Anime Listings
 Route::get('/anime/browse', 'AnimeController@search');
@@ -39,22 +58,3 @@ Route::get('/anime/{show}/{title?}', 'ShowController@details');
 Route::get('/anime/{show}/{title}/{translation_type}/episode-{episode_num}', 'EpisodeController@gotoEpisode');
 Route::get('/anime/{show}/{title}/{translation_type}/episode-{episode_num}/{streamer}/{mirror}', 'EpisodeController@episode');
 Route::get('/stream/{video}/video', 'EpisodeController@static');
-
-// Post Controller
-Route::post('/anime/add', 'PostController@showAdd');
-
-// Profile Pages
-Route::get('/anime/overview', 'UserController@overview'); //TODO
-Route::get('/user/settings', 'UserController@settings_general');
-Route::get('/user/notifications/mail', 'UserController@settings_nots_mail');
-
-// Profile Modifications
-Route::post('/user/settings/general', 'Auth\UpdateSettingsController@general');
-Route::post('/user/settings/mal', 'Auth\UpdateSettingsController@mal');
-Route::post('/user/settings/password', 'Auth\UpdateSettingsController@password');
-Route::post('/user/notifications/mail/general', 'Auth\UpdateSettingsController@notifications_mail_general');
-Route::post('/user/notifications/mail/specific', 'Auth\UpdateSettingsController@notifications_mail_specific');
-
-// CSS Routes
-Route::get('/user/settings/css', 'CssController@showUserSettings'); //TODO
-Route::post('/user/settings/css', 'CssController@setUserSettings'); //TODO
