ec1de496515f8bea5aebaa23e315c1ba28028270
Wilco de Boer <wfh@wilcodeboer.me>
2016-10-15 02:04:52 +0200

    Update a few controllers and pages

--- app/Http/Controllers/Auth/UpdateSettingsController.php
+++ app/Http/Controllers/Auth/UpdateSettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests;
+
 use Illuminate\Support\Facades\Auth;
 
 class UpdateSettingsController extends Controller
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+
 use Illuminate\Support\Facades\Auth;
 use App\Show;
 
