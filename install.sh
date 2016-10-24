c8e6f1739aa2d0e362d0ae7ae758c4c3172c071d
Wilco de Boer <wfh@wilcodeboer.me>
2016-10-24 17:45:38 +0200

    Try to make travis work

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+composer install
+npm install
+
+pip install cfscrape --upgrade
+pip install selenium --upgrade
+
+npm install phantomjs
--- installUser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-composer install
-update.sh
