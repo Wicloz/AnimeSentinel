bd171c2e6cb9a292682fc9ca0ba596ec0825337b
Wilco de Boer <wfh@wilcodeboer.me>
2016-10-26 00:48:11 +0200

    Add tests for the downloaders

--- app/AnimeSentinel/Python/GetExpanded.py
+++ app/AnimeSentinel/Python/GetExpanded.py
@@ -1,8 +1,10 @@
 import sys
+import time
 from selenium import webdriver
 
 if __name__ == "__main__":
   driver = webdriver.PhantomJS(service_log_path='/tmp/ghostdriver.log')
   driver.get(sys.argv[1])
+  time.sleep(2)
   print driver.execute_script("return document.documentElement.outerHTML")
   driver.quit()
--- tests/DownloadTest.php
+++ tests/DownloadTest.php
@@ -4,7 +4,19 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\AnimeSentinel\Downloaders;
+
 class DownloadTest extends TestCase
 {
+  public function testCloudflare() {
+    $this->assertContains('KissAnime - Watch anime online in high quality', Downloaders::downloadPage('http://kissanime.to/'));
+  }
 
+  // public function testJavascript() {
+  //   $this->assertContains('Your Anime community entertainment center.', Downloaders::downloadPage('https://htvanime.com/'));
+  // }
+
+  public function testScrolled() {
+    $this->assertContains('Zoku Natsume Yuujinchou: 3D Nyanko-sensei Gekijou', Downloaders::downloadPage('https://myanimelist.net/animelist/Wicloz'));
+  }
 }
