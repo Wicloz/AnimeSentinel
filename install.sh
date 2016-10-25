7dded0114ca04b0631bddd75edd072a54490e7cd
Wilco de Boer <wfh@wilcodeboer.me>
2016-10-25 16:42:42 +0200

    Change node stuff again

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-nvm install node
-
 composer create-project
 npm install
 
