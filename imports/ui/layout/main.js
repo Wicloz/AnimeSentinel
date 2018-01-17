e79a97bbb43aedd306092da8bc4229053f257a44
Wilco de Boer <wfh@wilcodeboer.me>
2018-01-17 22:22:13 +0100

    Clean up main template

--- imports/ui/layout/main.js
+++ imports/ui/layout/main.js
@@ -1,8 +1,2 @@
 import './main.html';
 import '/imports/ui/components/navMain.js';
-
-Template.layouts_main.helpers({
-  loadingBackground() {
-    return Session.get('LoadingBackground');
-  }
-});
