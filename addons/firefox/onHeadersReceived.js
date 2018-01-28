272418962f85cf1655758e938c983d352da09af6
Wilco de Boer <wfh@wilcodeboer.me>
2018-01-28 20:55:13 +0100

    Add CORS stuff to the addon

--- addons/firefox/onHeadersReceived.js
+++ addons/firefox/onHeadersReceived.js
@@ -1,14 +1,31 @@
 function modifyHeaders(response) {
-  response.responseHeaders = response.responseHeaders.filter((header) => {
-    return header.name.toLowerCase() !== 'x-frame-options';
-  });
+  // console.log(response);
 
-  // console.log(response.responseHeaders.map((header) => {
-  //   return header.name.toLowerCase();
-  // }));
-  return {
-    responseHeaders: response.responseHeaders
-  };
+  if (response.originUrl.match(/^.*:\/\/development.wilcodeboer.me/) || response.originUrl.match(/^.*:\/\/anime.wilcodeboer.me/)) {
+    response.responseHeaders = response.responseHeaders.filter((header) => {
+      return !['x-frame-options', 'access-control-allow-origin', 'access-control-allow-credentials', 'access-control-allow-methods', 'access-control-allow-headers'].includes(header.name.toLowerCase());
+    });
+
+    response.responseHeaders.push({
+      name: 'Access-Control-Allow-Origin',
+      value: response.originUrl.replace(/^(.*:\/\/[^\/]+)\/?.*$/, '$1')
+    });
+    response.responseHeaders.push({
+      name: 'Access-Control-Allow-Credentials',
+      value: 'true'
+    });
+    response.responseHeaders.push({
+      name: 'Access-Control-Allow-Methods',
+      value: 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH'
+    });
+    response.responseHeaders.push({
+      name: 'Access-Control-Allow-Headers',
+      value: 'Accept, Authorization, Cache-Control, Content-Type, DNT, If-Modified-Since, Keep-Alive, Origin, User-Agent, X-Requested-With'
+    });
+  }
+
+  // console.log(response.responseHeaders);
+  return {responseHeaders: response.responseHeaders};
 }
 
 browser.webRequest.onHeadersReceived.addListener(
