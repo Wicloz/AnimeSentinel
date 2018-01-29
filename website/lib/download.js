ad40f02948594cbde151be9735688a29893382f7
Wilco de Boer <wfh@wilcodeboer.me>
2018-01-29 16:31:13 +0100

    Only download with the plugin

--- website/lib/download.js
+++ website/lib/download.js
@@ -3,25 +3,27 @@ import {CloudKicker} from "cloudkicker/lib/index";
 const cloudkicker = new CloudKicker();
 
 downloadWithCallback = function(url, callback, tries=1) {
-  url = encodeURI(url).replace(/%25/g, '%');
+  if (Meteor.isServer || Session.get('AddOnInstalled')) {
+    url = encodeURI(url).replace(/%25/g, '%');
 
-  if (Meteor.isDevelopment) {
-    console.log('Downloading: url \'' + url + '\', try \'' + tries + '\'');
-  }
-
-  cloudkicker.get(url).then(({options, response}) => {
-    callback(response.body.toString());
-  }, (err) => {
-    if (tries >= 3) {
-      console.error('Failed downloading ' + url + ' after ' + tries + ' tries.');
-      console.error(err);
-      if (Meteor.isServer) {
-        callback(false);
-      }
-    } else {
-      downloadWithCallback(url, callback, tries + 1);
+    if (Meteor.isDevelopment) {
+      console.log('Downloading: url \'' + url + '\', try \'' + tries + '\'');
     }
-  }).catch((err) => {
-    console.error(err);
-  });
+
+    cloudkicker.get(url).then(({options, response}) => {
+      callback(response.body.toString());
+    }, (err) => {
+      if (tries >= 3) {
+        console.error('Failed downloading ' + url + ' after ' + tries + ' tries.');
+        console.error(err);
+        if (Meteor.isServer) {
+          callback(false);
+        }
+      } else {
+        downloadWithCallback(url, callback, tries + 1);
+      }
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
 };
