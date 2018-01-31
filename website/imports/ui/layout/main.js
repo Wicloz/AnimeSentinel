0bf09b662000af3977b0431637ad1936a65edab2
Wilco de Boer <wfh@wilcodeboer.me>
2018-01-31 22:54:36 +0100

    Add note to tell about this new site

--- website/imports/ui/layout/main.js
+++ website/imports/ui/layout/main.js
@@ -1,2 +1,26 @@
 import './main.html';
 import '/imports/ui/components/navMain.js';
+import * as RLocalStorage from 'meteor/simply:reactive-local-storage';
+
+Template.layouts_main.helpers({
+  superNoteDismissed() {
+    return RLocalStorage.getItem('SuperNoteDismissed');
+  }
+});
+
+Template.layouts_main.events({
+  'click .btn-dismiss'(event) {
+    RLocalStorage.setItem('SuperNoteDismissed', true);
+  },
+
+  'click .btn-oldsite-desktop'(event) {
+    $('.tap-target-desktop').tapTarget('open');
+  },
+
+  'click .btn-oldsite-mobile'(event) {
+    $('.button-collapse').sideNav('show');
+    setTimeout(function() {
+      $('.tap-target-mobile').tapTarget('open');
+    }, 300);
+  },
+});
