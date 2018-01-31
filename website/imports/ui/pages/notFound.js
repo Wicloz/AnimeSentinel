580c74cb5e5466f16616320b42b1e8ddf842f5fd
Wilco de Boer <wfh@wilcodeboer.me>
2018-01-31 19:51:00 +0100

    Improve welcome and not found pages

--- website/imports/ui/pages/notFound.js
+++ website/imports/ui/pages/notFound.js
@@ -3,3 +3,11 @@ import './notFound.html';
 Template.pages_notFound.onCreated(function() {
   Session.set('PageTitle', 'Page Not Found');
 });
+
+Template.pages_notFound.helpers({
+  breadCrumbsWithUrl() {
+    return JSON.parse(Session.get('BreadCrumbs')).filter((breadcrumb) => {
+      return breadcrumb.url;
+    });
+  }
+});
