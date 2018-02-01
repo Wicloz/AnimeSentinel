8039f5790abc900940fc1773ab93e3c8b13f9f76
Wilco de Boer <wfh@wilcodeboer.me>
2018-02-01 19:04:29 +0100

    Add page titles

--- website/imports/ui/layout/main.js
+++ website/imports/ui/layout/main.js
@@ -2,6 +2,15 @@ import './main.html';
 import '/imports/ui/components/navMain.js';
 import * as RLocalStorage from 'meteor/simply:reactive-local-storage';
 
+Template.layouts_main.onCreated(function() {
+  // TODO: Use a more general SEO solution
+  this.autorun(() => {
+    document.title = JSON.parse(Session.get('BreadCrumbs')).reduce((total, breadCrumb) => {
+      return total + breadCrumb.name + ' > ';
+    }, '') + Session.get('PageTitle')
+  });
+});
+
 Template.layouts_main.helpers({
   superNoteDismissed() {
     return RLocalStorage.getItem('SuperNoteDismissed');
