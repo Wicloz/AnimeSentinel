08415e0188466b0f450a25cf4a06f77dd9444223
Wilco de Boer <wfh@wilcodeboer.me>
2018-02-04 21:48:15 +0100

    Add template for streamer scrape information

---
+++ website/imports/streamers/__template.js
@@ -0,0 +1,164 @@
+export let template = {
+  // General data
+  id: '',
+  name: '',
+  homepage: '',
+  minimalPageTypes: [],
+
+  isInvalidPage(page) {
+    return false;
+  },
+
+  // Search page data
+  search: {
+    createUrl: function(query) {
+      return '';
+    },
+    rowSelector: '',
+    rowSkips: 0,
+
+    // Search page attribute data
+    attributes: {
+      streamerUrls: function(partial, full) {
+        return [{
+          type: '',
+          url: ''
+        }];
+      },
+      name: function(partial, full) {
+        return '';
+      },
+      altNames: function(partial, full) {
+        return [];
+      },
+      description: function(partial, full) {
+        return '';
+      },
+      type: function(partial, full) {
+        return '';
+      },
+      malId: function(partial, full) {
+        return '';
+      },
+    },
+
+    // Search page thumbnail data
+    thumbnails: {
+      rowSelector: '',
+      getUrl: function (partial, full) {
+        return '';
+      },
+    },
+  },
+
+  // Show page data
+  show: {
+    checkIfPage: function(page) {
+      return false;
+    },
+
+    // Show page attribute data
+    attributes: {
+      streamerUrls: function(partial, full) {
+        return [{
+          type: '',
+          url: ''
+        }];
+      },
+      name: function(partial, full) {
+        return '';
+      },
+      altNames: function(partial, full) {
+        return [];
+      },
+      description: function(partial, full) {
+        return '';
+      },
+      type: function(partial, full) {
+        return '';
+      },
+      malId: function(partial, full) {
+        return '';
+      },
+    },
+
+    // Show page thumbnail data
+    thumbnails: {
+      rowSelector: '',
+      getUrl: function (partial, full) {
+        return '';
+      },
+    },
+  },
+
+  // Related shows data
+  showRelated: {
+    rowSelector: '',
+    rowIgnore: function(partial) {
+      return false;
+    },
+
+    // Related shows attribute data
+    attributes: {
+      streamerUrls: function(partial, full) {
+        return [{
+          type: '',
+          url: ''
+        }];
+      },
+      name: function(partial, full) {
+        return '';
+      },
+      altNames: function(partial, full) {
+        return [];
+      },
+      description: function(partial, full) {
+        return '';
+      },
+      type: function(partial, full) {
+        return '';
+      },
+      malId: function(partial, full) {
+        return '';
+      },
+    },
+
+    // Related shows thumbnail data
+    thumbnails: {
+      rowSelector: '',
+      getUrl: function (partial, full) {
+        return '';
+      },
+    },
+  },
+
+  // Episode list data
+  showEpisodes: {
+    rowSelector: '',
+    rowSkips: 0,
+    cannotCount: false,
+
+    // Episode list attribute data
+    attributes: {
+      episodeNumStart: function(partial, full) {
+        return '';
+      },
+      episodeNumEnd: function(partial, full) {
+        return '';
+      },
+      translationType: function(partial, full) {
+        return '';
+      },
+      sourceUrl: function(partial, full) {
+        return '';
+      },
+      sources: function(partial, full) {
+        return [{
+          name: '',
+          url: '',
+          flags: []
+        }];
+      },
+    },
+  },
+};
