ad835f30c259d7b047e300a8ba86b72469a17eda
Wilco de Boer <wfh@wilcodeboer.me>
2018-02-08 12:08:16 +0100

    Show spinner when images are downloading

--- website/imports/ui/components/image.js
+++ website/imports/ui/components/image.js
@@ -5,6 +5,10 @@ Template.components_image.helpers({
     return Template.instance().state.get('appeared');
   },
 
+  getNotLoaded() {
+    return !Template.instance().state.get('loaded');
+  },
+
   getId() {
     return Template.instance().state.get('id');
   },
@@ -22,14 +26,32 @@ Template.components_image.onCreated(function () {
   this.state = new ReactiveDict();
   this.state.setDefault({
     appeared: false,
+    loaded: false,
     id: undefined
   });
+
+  this.afterLoad = () => {
+    this.state.set('loaded', true);
+  };
 
   this.autorun(() => {
     if (Template.currentData().id) {
       this.state.set('id', Template.currentData().id);
     } else {
       this.state.set('id', createUniqueId());
+    }
+  });
+
+  this.autorun(() => {
+    if (this.state.get('appeared')) {
+      Tracker.afterFlush(() => {
+        let img  = $('#' + this.state.get('id')).get(0);
+        if (img.complete) {
+          this.afterLoad();
+        } else {
+          img.addEventListener('load', this.afterLoad);
+        }
+      });
     }
   });
 
