fef440b782d868c3f7850bdc344fba3c3d7cde59
Wilco de Boer <wfh@wilcodeboer.me>
2018-04-17 16:54:00 +0200

    Add users collection

---
+++ website/imports/api/users/users.js
@@ -0,0 +1,119 @@
+import SimpleSchema from 'simpl-schema';
+
+// Schema
+Schemas.User = new SimpleSchema({
+  _id: {
+    type: String,
+    optional: true
+  },
+  createdAt: {
+    type: Date
+  },
+  services: {
+    type: Object,
+    blackbox: true
+  },
+
+  emails: {
+    type: Array,
+    minCount: 1
+  },
+  'emails.$': {
+    type: Object
+  },
+  'emails.$.address': {
+    type: String,
+    regEx: SimpleSchema.RegEx.EmailWithTLD
+  },
+  'emails.$.verified': {
+    type: Boolean
+  },
+
+  profile: {
+    type: Object,
+    optional: true
+  },
+  'profile.username': {
+    type: String
+  },
+  'profile.malUsername': {
+    type: String,
+    optional: true,
+    label: 'MAL Username'
+  },
+  'profile.malPassword': {
+    type: String,
+    optional: true,
+    label: 'MAL Password',
+    autoform: {
+      type: 'password'
+    }
+  }
+}, { tracker: Tracker });
+
+Schemas.UserClient = new SimpleSchema({
+  'emails': Schemas.User._schema['emails'],
+  'emails.$': Schemas.User._schema['emails.$'],
+  'emails.$.address': Schemas.User._schema['emails.$.address'],
+  'profile': Schemas.User._schema['profile'],
+  'profile.username': Schemas.User._schema['profile.username'],
+  'profile.malUsername': Schemas.User._schema['profile.malUsername'],
+  'profile.malPassword': Schemas.User._schema['profile.malPassword'],
+}, { tracker: Tracker });
+
+Meteor.users.attachSchema(Schemas.User);
+
+// Helpers
+Meteor.users.helpers({
+  changeInfo(newInfo) {
+    let oldEmailAddresses = this.emails.pluck('address');
+
+    // Mark changed email addresses as unverified
+    newInfo.emails = newInfo.emails.map((email) => {
+      if (oldEmailAddresses.includes(email.address)) {
+        email.verified = this.emails.getPartialObjects({
+          address: email.address
+        })[0].verified;
+      } else {
+        email.verified = false;
+      }
+      return email;
+    });
+
+    // Update this
+    Object.keys(newInfo).forEach((key) => {
+      this[key] = newInfo[key];
+    });
+
+    // Update database
+    Meteor.users.update(this._id, {
+      $set: Schemas.User.clean(this, {
+        mutate: true
+      })
+    });
+
+    // Send verification mails
+    newInfo.emails.forEach((email) => {
+      if (Meteor.isServer && !oldEmailAddresses.includes(email.address)) {
+        Accounts.sendVerificationEmail(this._id, email.address);
+      }
+    });
+  }
+});
+
+Meteor.users.changeUserInfo = function(userId, newInfo) {
+  Schemas.id.validate({
+    id: userId
+  });
+  Schemas.UserClient.validate(newInfo);
+
+  let user = Meteor.users.findOne(userId);
+  user.changeInfo(newInfo);
+};
+
+// Methods
+Meteor.methods({
+  'users.updateCurrentUser'(user) {
+    Meteor.users.changeUserInfo(this.userId, user);
+  }
+});
