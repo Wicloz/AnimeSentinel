5f5cbff54a44e4274412e52834c5f028869bcd3a
Wilco de Boer <wfh@wilcodeboer.me>
2018-04-17 16:52:48 +0200

    Add routes for users

--- website/startup/client/routes.js
+++ website/startup/client/routes.js
@@ -5,6 +5,7 @@ import '/imports/ui/pages/notFound.js';
 import '/imports/ui/pages/show.js';
 import '/imports/ui/pages/episode.js';
 import '/imports/ui/pages/recent.js';
+import '/imports/ui/pages/profile.js';
 
 FlowRouter.notFound = {
   name: 'notFound',
@@ -52,5 +53,63 @@ FlowRouter.route('/recent', {
   name: 'recent',
   action() {
     BlazeLayout.render('layouts_main', {content: 'pages_recent'});
+  }
+});
+
+FlowRouter.route('/sign-out', {
+  name: 'logOut',
+  triggersEnter: [AccountsTemplates.ensureSignedIn],
+  action() {
+    AccountsTemplates.logout();
+  }
+});
+
+FlowRouter.route('/manage-profile', {
+  name: 'manageProfile',
+  triggersEnter: [AccountsTemplates.ensureSignedIn],
+  action() {
+    BlazeLayout.render('layouts_main', {content: 'pages_profile'});
+  },
+});
+
+AccountsTemplates.configureRoute('signIn', {
+  name: 'logIn',
+  redirect: 'manageProfile'
+});
+
+AccountsTemplates.configureRoute('signUp', {
+  name: 'register',
+  redirect: 'manageProfile'
+});
+
+AccountsTemplates.configureRoute('forgotPwd', {
+  name: 'forgotPassword',
+  redirect: 'forgotPassword'
+});
+
+AccountsTemplates.configureRoute('resetPwd', {
+  name: 'resetPassword',
+  redirect: function() {
+    if (Meteor.user()) {
+      FlowRouter.go('manageProfile');
+    } else {
+      FlowRouter.go('logIn');
+    }
+  }
+});
+
+AccountsTemplates.configureRoute('resendVerificationEmail', {
+  name: 'resendVerificationEmail',
+  redirect: 'resendVerificationEmail'
+});
+
+AccountsTemplates.configureRoute('verifyEmail', {
+  name: 'verifyEmail',
+  redirect: function() {
+    if (Meteor.user()) {
+      FlowRouter.go('manageProfile');
+    } else {
+      FlowRouter.go('logIn');
+    }
   }
 });
