0f35c286933dd700eb3904277fd37a22756a5295
Wilco de Boer <wfh@wilcodeboer.me>
2018-05-08 22:23:32 +0200

    Add schema for statuses selection

--- website/lib/schemas.js
+++ website/lib/schemas.js
@@ -3,28 +3,3 @@ import SimpleSchema from 'simpl-schema';
 SimpleSchema.extendOptions(['autoform']);
 
 Schemas = {};
-
-Schemas.id = new SimpleSchema({
-  id: {
-    type: String
-  }
-}, { tracker: Tracker });
-
-Schemas.ids = new SimpleSchema({
-  ids: {
-    type: Array
-  },
-  'ids.$': {
-    type: String
-  }
-}, { tracker: Tracker });
-
-Schemas.episodeSelection = new SimpleSchema({
-  episodeNumber: {
-    type: String,
-    autoform: {
-      label: 'Select Episode:',
-      firstOption: false
-    }
-  }
-}, { tracker: Tracker });
