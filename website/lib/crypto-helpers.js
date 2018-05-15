6cc8fb6fc6e4adf3d7bb4170866d2bfe4bdb19bb
Wilco de Boer <wfh@wilcodeboer.me>
2018-05-15 16:38:02 +0200

    Add functions to decode and encode to/from base 64

--- website/lib/crypto-helpers.js
+++ website/lib/crypto-helpers.js
@@ -7,3 +7,15 @@ createWeakHash = function (data) {
 createUniqueId = function() {
   return createWeakHash(Date.now() + Math.random());
 };
+
+encodeBase64 = function(str) {
+  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => {
+    return String.fromCharCode('0x' + p1);
+  }));
+};
+
+decodeBase64 = function(str) {
+  return decodeURIComponent(atob(str).split('').map((c) => {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''));
+};
