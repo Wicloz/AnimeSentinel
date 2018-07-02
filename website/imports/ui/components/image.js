7051e718ff72659b386f886f7482dd81426f7c85
Wilco de Boer <wfh@wilcodeboer.me>
2018-07-02 16:37:57 +0200

    Fix image component

--- website/imports/ui/components/image.js
+++ website/imports/ui/components/image.js
@@ -12,62 +12,61 @@ Template.components_image.helpers({
   getId() {
     return Template.instance().state.get('id');
   },
+
+  getSrc() {
+    return Template.instance().state.get('src');
+  }
 });
 
 Template.components_image.events({
-  'appear imgdet'(event) {
-    if ($(event.target).attr('id') === 'image-detector-' + Template.instance().state.get('id')) {
+  'appear .img-detector'(event) {
+    if ($(event.target).attr('id') === 'img-detector-' + Template.instance().state.get('id')) {
       Template.instance().state.set('appeared', true);
+    }
+  },
+
+  'load .img-lazy'(event) {
+    if ($(event.target).attr('id') === Template.instance().state.get('id')) {
+      Template.instance().state.set('loaded', true);
     }
   }
 });
 
 Template.components_image.onCreated(function () {
+  // Local variables
   this.state = new ReactiveDict();
   this.state.setDefault({
     appeared: false,
     loaded: false,
-    id: undefined
+    id: undefined,
+    src: Template.currentData().src
   });
 
+  // When the id changes
   this.autorun(() => {
     if (Template.currentData().id) {
       this.state.set('id', Template.currentData().id);
-    } else {
+    } else if (!this.state.get('id')) {
       this.state.set('id', createUniqueId());
     }
   });
 });
 
 Template.components_image.onRendered(function () {
+  // When the id changes
   this.autorun(() => {
-    if (this.state.get('appeared')) {
-      let temp = Template.currentData().src;
-      Tracker.afterFlush(() => {
-        let img = $('#' + this.state.get('id')).get(0);
-        if (img) {
-          if (img.complete) {
-            this.state.set('loaded', true);
-          } else {
-            this.state.set('loaded', false);
-            img.addEventListener('load', () => {
-              this.state.set('loaded', true);
-            });
-          }
-        }
-      });
-    }
+    $('#img-detector-' + this.state.get('id')).appear();
   });
 
+  // When the src changes
   this.autorun(() => {
-    if (this.state.get('appeared') && Template.currentData().class.split(' ').includes('materialboxed')) {
-      let temp = Template.currentData().caption;
-      Tracker.afterFlush(() => {
-        $('#' + this.state.get('id')).materialbox();
-      });
+    if (Template.currentData().src !== this.state.get('src')) {
+      this.state.set('appeared', false);
+      this.state.set('loaded', false);
+      this.state.set('src', Template.currentData().src);
+      Tracker.nonreactive(() => {
+        $.force_appear();
+      })
     }
   });
-
-  $('#image-detector-' + this.state.get('id')).appear();
-  $.force_appear();
 });
