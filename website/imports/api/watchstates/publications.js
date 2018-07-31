cbdea67f81fa3272cb3dfb8c0f3f99c85834878d
Wilco de Boer <wfh@wilcodeboer.me>
2018-07-31 20:07:05 +0200

    Improve watchState publications

--- website/imports/api/watchstates/publications.js
+++ website/imports/api/watchstates/publications.js
@@ -1,13 +1,25 @@
 import { WatchStates } from './watchstates.js';
 
 Meteor.publish('watchStates.currentUserUnique', function(malId) {
+  if (!this.userId) {
+    return this.ready();
+  }
+
   return WatchStates.queryUnique(this.userId, malId);
 });
 
 Meteor.publish('watchStates.currentUserUniqueMultiple', function(malIds) {
+  if (!this.userId) {
+    return this.ready();
+  }
+
   return WatchStates.queryUniqueMultiple(this.userId, malIds);
 });
 
 Meteor.publish('watchStates.currentUserWithStatuses', function(statuses) {
+  if (!this.userId) {
+    return this.ready();
+  }
+
   return WatchStates.queryWithStatuses(this.userId, statuses);
 });
