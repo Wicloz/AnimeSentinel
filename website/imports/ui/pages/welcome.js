619d7642e3581f88a674b882048f1572ac10adf1
Wilco de Boer <deboer.wilco@gmail.com>
2019-03-19 17:01:51 +0100

    Improve the welcome page a bit

--- website/imports/ui/pages/welcome.js
+++ website/imports/ui/pages/welcome.js
@@ -4,3 +4,12 @@ Template.pages_welcome.onCreated(function() {
   Session.set('BreadCrumbs', JSON.stringify([]));
   Session.set('PageTitle', 'AnimeSentinel');
 });
+
+Template.pages_welcome.helpers({
+  queryRecent() {
+    return {
+      sortBy: 'Latest Update',
+      sortDirection: -1
+    };
+  }
+});
