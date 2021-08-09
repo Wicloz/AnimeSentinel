27c8d7e9051d5704c39b4288a94708a31bcbc9c4
Wilco de Boer <deboer.wilco@gmail.com>
2021-08-09 21:41:58 +0200

    Revert "Remove KissAnime support"
    This reverts commit d2618ac208cd0a0b5686c1b61978e7ce4cf055db.

--- website/imports/streamers/streamers.js
+++ website/imports/streamers/streamers.js
@@ -1,13 +1,14 @@
 import Cheerio from 'cheerio';
 import { Shows } from '/imports/api/shows/shows.js';
 import { myanimelist } from './_myanimelist';
+import { kissanime } from './_kissanime';
 import { nineanime } from './_nineanime';
 import {Searches} from '../api/searches/searches';
 import ScrapingHelpers from './scrapingHelpers';
 import moment from 'moment-timezone';
 import {Episodes} from '../api/episodes/episodes';
 
-let streamers = [myanimelist, nineanime];
+let streamers = [myanimelist, kissanime, nineanime];
 
 export default class Streamers {
   static getStreamers() {
